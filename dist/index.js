5d536107b6aa5fe73694b281d9de743135eecc4f
Mateusz Urban <98m.urban@gmail.com>
2020-09-08 17:25:55 +0200

    better mobile friendly look

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ let info = document.querySelector(".error")
 const loadingTag = document.querySelector(".loading");
 const fav = document.querySelectorAll(".addToFav");
 const favButton = document.querySelector(".favorite");
+const fakeSection = document.querySelector(".fake")
 
 const API = `https://api.publicapis.org/entries?category`;
 button.addEventListener('click', getApis);
@@ -25,12 +26,12 @@ async function getApis() {
     info.innerHTML = "";
     let apiCategories = category.value;
     displaySpinner();
-
+    
     await fetch(`${API}=${apiCategories}&https=true`)
-        .then(apisLi => data = apisLi.json())
-        .then(data => {
-            let entries = data.entries;
-
+    .then(apisLi => data = apisLi.json())
+    .then(data => {
+        let entries = data.entries;
+        
             hideSpinner();
             entries.forEach(el => {
                 el.Auth === "" ? el.Auth = 'No Auth' : displayApi(el);
@@ -41,63 +42,74 @@ async function getApis() {
                 info.innerText = `Please Select Category`;
             }
         })
-
-}
-
-const displayApi = (item) => {
-    const {
-        API,
-        Auth,
-        Description,
-        Link
-    } = item;
-
-    let cardHTML = `
-    <div class="api">
-    <div class="api__wrapper">
-    <h3 class="api__title">${API}</h3>
-    <i id="addToFav"class=" far fa-star "></i>
-    <p class="api__auth"><span>Auth</span>: ${Auth}</p>
-    <p class="api__descryption">${Description}</p>
-    </div>
-    <a href=${Link} class="doc__link" target="_blanck"><i class="fas fa-external-link-alt"></i></a>
-    </div>`;
-    displayedApi.insertAdjacentHTML('beforeend', cardHTML);
-}
-
-// save item into local storage to be later displayed in the Favorite section
-document.body.addEventListener('click', function (e) {
-    if (event.srcElement.id == 'addToFav') {
-        if (typeof (Storage) !== "undefined") {
-            let apiCard = e.target.parentElement.parentElement.innerHTML;
-            console.log(apiCard)
-            const getApiTitle = e.target.parentElement.children[0].innerText;
-            e.target.classList.add("fas")
-            localStorage.setItem(getApiTitle, apiCard);
-        } else {
-            alert("Storage in this browser is not supported")
-        }
-    };
-});
-
-function getFavfomLocalStrg() {
-    let favHTML = [];
-    let keys = Object.keys(localStorage);
-    let i = keys.length;
-    while (i--) {
-        favHTML.push(localStorage.getItem(keys[i]))
+        
     }
-    return favHTML
-}
+    
+    const displayApi = (item) => {
+        const {
+            API,
+            Auth,
+            Description,
+            Link
+        } = item;
+        
+        let cardHTML = `
+        <div class="api">
+        <div class="api__wrapper">
+        <h3 class="api__title">${API}</h3>
+        <i id="addToFav"class=" far fa-star "></i>
+        <p class="api__auth"><span>Auth</span>: ${Auth}</p>
+        <p class="api__descryption">${Description}</p>
+        </div>
+        <a href=${Link} class="doc__link" target="_blanck"><i class="fas fa-external-link-alt"></i></a>
+        </div>`;
+        displayedApi.insertAdjacentHTML('beforeend', cardHTML);
+    }
+    
+    // save item into local storage to be later displayed in the Favorite section
+    document.body.addEventListener('click', function (e) {
+        if (event.srcElement.id == 'addToFav') {
+            if (typeof (Storage) !== "undefined") {
+                let apiCard = e.target.parentElement.parentElement.innerHTML;
+                const getApiTitle = e.target.parentElement.children[0].innerText;
+                e.target.classList.add("fas")
+                localStorage.setItem(getApiTitle, apiCard);
+            } else {
+                alert("Storage in this browser is not supported")
+            }
+        };
+    });
+    
+    function getFavfomLocalStrg() {
+        let favHTML = [];
+        let keys = Object.keys(localStorage);
+        let i = keys.length;
+        while (i--) {
+            favHTML.push(localStorage.getItem(keys[i]))
+        }
+        return favHTML
+    }
+    
+    function createApiDiv(num) {
+        let z = document.createElement("div");
+        z.classList.add("api")
+        fakeSection.parentElement.appendChild(z);
+    }
+    // create as much div as it is saved elements in localSotrage 
+    (function repeat(num) {
+        createApiDiv(num);
+        if (num > 1) repeat(num - 1);
+    })(localStorage.length);
+    
+    const apiSection = document.querySelectorAll(".api");
 
-displayedApi.insertAdjacentHTML('beforeend', '<div class="api"></div>');
-let apiSection = document.querySelector(".api");
-
-function showFavorite() {
-    favButton.addEventListener("click", function () {
-        apiSection.insertAdjacentHTML('beforebegin', getFavfomLocalStrg())
-   
+    function showFavorite() {
+        favButton.addEventListener("click", function () {
+            if (localStorage.length === 0) {
+                info.innerText = "no cards added to favorites"};
+                for (let i = 0; i <= apiSection.length; i++) {
+        apiSection[i].insertAdjacentHTML('afterbegin', getFavfomLocalStrg());
+        }
     })
-
 }
 showFavorite();
